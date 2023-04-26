c0214935144828f11349e6fbbca7e00fffcb99ac
Adailsonxtx <100886967+Adailsonxtx@users.noreply.github.com>
2023-04-25 23:02:25 -0300

    MeuCodigo
    Desenvolvimento HTML , CSS

---
+++ admin/formulario/Inicio.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Aluguel de motos</title>
+	<link rel="stylesheet" type="text/css" href="../../css/adailson.css">
+</head>
+<body>
+	<header>
+		<h1>Aluguel de motos</h1>
+		<nav>
+			<div>
+				<img src="../../img/adailson.png" class="logo_adailson">
+			</div>
+
+			<ul>
+				<li><a href="#">Login</a></li>
+				<li><a href="#">Cadastro</a></li>
+				<li><a href="#">Contato</a></li>
+			</ul>
+		</nav>
+	</header>
+	<main>
+		<section id="banner">
+
+			<h2>Alugue uma moto agora</h2>
+			
+			<a href="#" class="button">Ver motos</a>
+		</section>
+		<section id="moto-list">
+			<h2>Motos disponíveis</h2>
+			<ul>
+				<li>
+					<img src="../../img/fan.png" alt="Moto 1">
+					<h3>CG FAN 160</h3>
+					<p>Qualidade e confiança</p>
+					<a href="#" class="button">Alugar</a>
+				</li>
+				<li>
+					<img src="../../img/pop.png" alt="Moto 2">
+					<h3>POP 110i</h3>
+					<p>Economica e leve</p>
+					<a href="#" class="button">Alugar</a>
+				</li>
+				<li>
+					<img src="../../img/xre.png" alt="Carro 3">
+					<h3>XRE 300</h3>
+					<p>Offroad e Street</p>
+					<a href="#" class="button">Alugar</a>
+				</li>
+			</ul>
+		</section>
+	</main>
+	<footer>
+		<p>&copy; 2023 Aluguel de motos</p>
+	</footer>
+</body>
+</html>
---
+++ admin/formulario/cadastro.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+
+<html lang="pt-br">
+
+<head>
+
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Cadastro Cliente</title>
+
+	<!-- ARQUIVO DE ESTILOS DO PORTAL -->
+	<link rel="stylesheet" type="text/css" href="../../css/estilo.css">
+	
+</head>
+
+
+<body>
+
+		<form id="form_login" name="form_login" class="form_login" method="post" action="valida_login.php">
+
+			<div><h1>CADASTRO</h1></div>
+
+				<div class="agrupamento_login">
+
+					<div>
+
+						<div><label>Digite seu nome completo</label></div>	
+
+						<div><input type="text" id="nome_login" name="nome_login" required autofocus></div>
+
+						<div><label>Digite seu CPF</label></div>
+
+						<div><input type="password" id="senha_login" name="senha_login" required></div>
+
+						<div><label>Digite sua data de nascimento</label></div>
+
+						<div><input type="password" id="senha_login" name="senha_login" required></div>
+
+						<div><label>Digite seu RG</label></div>
+
+						<div><input type="password" id="senha_login" name="senha_login" required></div>
+
+						<div><label>Digite seu e-mail</label></div>
+
+						<div><input type="password" id="senha_login" name="senha_login" required></div>
+
+						<div><label>Crie uma senha</label></div>
+
+						<div><input type="password" id="senha_login" name="senha_login" required></div>
+
+						<div><label>Confirme sua senha</label></div>
+
+						<div><input type="password" id="senha_login" name="senha_login" required></div>
+
+						<div><input type="submit" id="btn_entrar" name="btn_entrar" value="Entrar"></div>
+
+
+					</div>
+
+					<div>
+
+						<img src="../../img/adailson.png" class="logo_login">
+
+					</div>
+
+
+				</div>
+
+		</form>
+</body>
+
+</html>
